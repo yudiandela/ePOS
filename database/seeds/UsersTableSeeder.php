a0755250ce11c171f2379fa213e01e91eeca7c54
Yudi Andela <yudhi.andhela@gmail.com>
2019-09-12 21:06:41 +0700

    Membuat data seeder untuk table users

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        return User::create([
+            'name'     => 'Administrator',
+            'email'    => 'admin@example.com',
+            'password' => Hash::make('12345678'),
+        ]);
+    }
+}
